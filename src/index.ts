a549fc8700f39a1bd75b7ed447731b2e5c81220c
Brad Henderson <elliotbradly@gmail.com>
2026-08-03 01:28:20 -0400

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
-/* * JSON TYPES and UTILS * */
-import { dateUtils, dayjs, numeric } from '@snailicid3/utils'
-export type HelloWorld = number | string
-export const helloThere: string = 'i am new'
-export const sampleFunc = (value: HelloWorld): HelloWorld => {
-    const __firs = dayjs('10-31-2004').isValid()
-    /** Tg.isNotNilOrEmpty('') */
-    const _result = dateUtils.isValidDate('10-31-2004', 'MM-DD-YYYY')
-    const semver = numeric.isNumeric('1')
-    console.log('sampleFunc:: ', value, semver, _result, __firs)
-    return value
-}
+// This is your entry file! Refer to it when you render:
+// npx remotion render <entry-file> HelloWorld out/video.mp4
 
-sampleFunc('herp derp')
+import { registerRoot } from "remotion";
+import { RemotionRoot } from "./Root";
+
+registerRoot(RemotionRoot);
